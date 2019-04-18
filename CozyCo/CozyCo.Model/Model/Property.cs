8e97eb956d35ba61e03a0c65b3cee83e29aa6e93
Erik Zambrano <erik@austincodingacademy.com>
2019-04-17 21:08:04 -0500

    add, detail, and delete actions

---
+++ CozyCo/CozyCo.Model/Model/Property.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CozyCo.Models
+{
+    public class Property
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+
+        [Display(Name ="Address cont.")]
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ CozyCo/CozyCo/Controllers/PropertyController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CozyCo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CozyCo.WebUI.Controllers
+{
+    public class PropertyController : Controller
+    {
+        private List<Property> Properties = new List<Property>
+        {
+            new Property { Id = 1, Address = "123 Main Street", City = "Austin", Zipcode = "78654" },
+            new Property { Id = 2, Address = "87887 Main Street", City = "Pflugerville", Zipcode = "78660"},
+        };
+
+        // property/index
+        public IActionResult Index()
+        {
+            return View(Properties);
+        }
+
+        // GET property/add
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Property newProperty) // -> receive data from a HTML form
+        {
+            Properties.Add(newProperty);
+
+            return View(nameof(Index), Properties);
+        }
+
+        public IActionResult Detail(int id) // -> get id from URL
+        {
+            var property = Properties.Single(p => p.Id == id);
+            
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var property = Properties.Single(p => p.Id == id);
+
+            Properties.Remove(property);
+
+            return View(nameof(Index), Properties);
+        }
+    }
+}
